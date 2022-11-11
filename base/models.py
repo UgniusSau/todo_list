338425be8e9cdda9ec41adb6f3685b51e5507e76
UgniusS <ugnius.saulevicius@gmail.com>
2022-11-11 22:23:07 +0200

    feat: change task primary key int ID to uuid

--- base/models.py
+++ base/models.py
@@ -1,9 +1,12 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 
 
 class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,8 @@ urlpatterns =[
     path('register/', RegisterPage.as_view(), name='register'),
 
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/<uuid:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+    path('task-update/<uuid:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<uuid:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
